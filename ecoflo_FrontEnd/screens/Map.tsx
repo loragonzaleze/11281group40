c1b8e49a8406ab5adb9a5ed80c5cf3815315e98d
loragonzaleze <61980534+loragonzaleze@users.noreply.github.com>
2021-11-13 02:25:45 -0500

    Map can now detect user location. Changed to use google maps for use when locating waste management near user

--- ecoflo_FrontEnd/screens/Map.tsx
+++ ecoflo_FrontEnd/screens/Map.tsx
@@ -1,27 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {useState} from 'react'
+import React, { useEffect } from 'react';
 
 import { StyleSheet, Text, View, TextInput, Button, Dimensions} from 'react-native';
-import axios from 'axios'
-import { NativeStackNavigationProp} from '@react-navigation/native-stack'
-import { RootStackParamList } from '../App';
 import styles from "../stylesFolder/styles"
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { useContext } from 'react';
-import MapView from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import {useState} from 'react'
+
+
+
 
 const Map = () => {
 
+    const [latitude, setLatitude] = useState(0.0)
+    const [longitude, setLongitude] = useState(0.0)
+    const [located, setLocated] = useState(false)
+
+   
+    useEffect(() => {
+        getLocation()
+    })
+    
+    async function getLocation() {
+      
+        let location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.Highest})
+        let latitude_ = location.coords.latitude
+        let longitude_ = location.coords.longitude
+        let altitude = location.coords.altitude
+        
+        setLatitude(latitude_)
+        setLongitude(longitude_)
+        setLocated(true)
+        console.log("Latitude: " + latitude + " Longitude: " + longitude + " Altitude: " + altitude)
+    }
+
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} />
-            <Marker
-                coordinate={{latitude : 29.653865 , longitude : -82.343551}}
-                image={{uri: 'icon'}}
-            />
+            {located ? 
+            <MapView 
+                style={styles.map} 
+                provider={PROVIDER_GOOGLE}
+                onPoiClick={(data) => {
+                    console.log(data)
+                }}
+                initialRegion={{
+                    latitude: latitude,
+                    longitude: longitude,
+                    latitudeDelta: 0.02,
+                    longitudeDelta: 0.01
+                }}>
+                    <Marker
+                        coordinate={{latitude : latitude , longitude :longitude}}
+                        pinColor={'red'}
+                    />
+            </MapView> :  
+            <MapView style={styles.map}/>}
         </View>
         
     );
